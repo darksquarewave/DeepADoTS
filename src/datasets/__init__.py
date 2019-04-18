6954809e1948f3e3110aaa9060ed3ed121f47def
az22 <xenoglaux.loweryi@gmail.com>
2019-04-18 15:49:32 +0200

    changes

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,16 +1,7 @@
 from .dataset import Dataset
-from .kdd_cup import KDDCup
-from .real_datasets import RealDataset, RealPickledDataset
-from .synthetic_data_generator import SyntheticDataGenerator
-from .synthetic_dataset import SyntheticDataset
-from .multivariate_anomaly_function import MultivariateAnomalyFunction
+from .pandas import PandasDataset
 
 __all__ = [
     'Dataset',
-    'SyntheticDataset',
-    'RealDataset',
-    'RealPickledDataset',
-    'KDDCup',
-    'SyntheticDataGenerator',
-    'MultivariateAnomalyFunction'
+    'PandasDataset'
 ]
